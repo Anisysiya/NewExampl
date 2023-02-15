806436d39d59c2379e1d7f1d698ce3986d50d852
Anisya <anisiya@list.ru>
2023-02-16 01:16:29 +0300

    Exp 005

---
+++ Exp005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username =="Анися")
+{
+    Console.WriteLine("Ура, это же Анися!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
